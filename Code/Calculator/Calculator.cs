656c4f912fc76d321fa0488d73b8f8fb164a3dae
Ion Dormenco <idormenco@gmail.com>
2023-11-08 17:11:53 +0200

    initial commit

---
+++ Code/Calculator/Calculator.cs
@@ -0,0 +1,59 @@
+﻿namespace Cucumba.Demo.Calculator;
+public static class Dear
+{
+    private class CalculatorImplementation : IAmCalculator
+    {
+        private double _result;
+
+        public static CalculatorImplementation New => new();
+        private CalculatorImplementation()
+        {
+            _result = 0d;
+        }
+
+        public IAmCalculator Add(double value)
+        {
+            _result += value;
+            return this;
+        }
+
+        public IAmCalculator Subtract(double value)
+        {
+            _result -= value;
+            return this;
+        }
+
+        public IAmCalculator MultiplyBy(double value)
+        {
+            _result *= value;
+            return this;
+        }
+
+        public IAmCalculator DivideBy(double value)
+        {
+            if (value == 0)
+            {
+                throw new InvalidOperationException("Division by zero is not allowed.");
+            }
+
+            _result /= value;
+            return this;
+        }
+
+        public double Please()
+        {
+            return _result;
+        }
+    }
+
+    public static IAmCalculator Calculator => CalculatorImplementation.New;
+}
+
+public interface IAmCalculator
+{
+    IAmCalculator Add(double value);
+    IAmCalculator Subtract(double value);
+    IAmCalculator MultiplyBy(double value);
+    IAmCalculator DivideBy(double value);
+    double Please();
+}
---
+++ Code/Pizzeria/Pizza.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.ObjectModel;
+using Spectre.Console;
+
+namespace Cucumba.Demo.Pizzeria;
+
+public class Fac
+{
+    public class O
+    {
+        private class PizzaMakerImplementation : IAmPizzaMaker
+        {
+            private readonly Stack<string> _ingredients;
+            private PizzaMakerImplementation()
+            {
+                _ingredients = new Stack<string>();
+            }
+
+            public static IAmPizzaMaker New => new PizzaMakerImplementation();
+            public IAmPizzaMaker CuBlat(Blat blat)
+            {
+                _ingredients.Push($"Blat {blat}");
+                return this;
+            }
+
+            public IAmPizzaMaker PunSosDeRosii()
+            {
+                _ingredients.Push("Sos rossi");
+                return this;
+            }
+
+            public IAmPizzaMaker PunMozzarella()
+            {
+                _ingredients.Push("Sos mozzarella");
+                return this;
+            }
+
+            public IAmPizzaMaker Pun(Topping topping)
+            {
+                _ingredients.Push(topping.ToString());
+                return this;
+            }
+
+            public Pizza BagLaCuptor()
+            {
+                return new Pizza(_ingredients);
+            }
+        }
+
+        public static IAmPizzaMaker Pizza { get; } = PizzaMakerImplementation.New;
+    }
+}
+
+
+public interface IAmPizzaMaker
+{
+    IAmPizzaMaker CuBlat(Blat blat);
+    IAmPizzaMaker PunSosDeRosii();
+    IAmPizzaMaker PunMozzarella();
+    IAmPizzaMaker Pun(Topping topping);
+    Pizza BagLaCuptor();
+}
+
+public enum Blat
+{
+    Subtire,
+    Pufos
+}
+
+public enum Topping
+{
+    Pepperoni,
+    Mozzarella,
+    Ciuperci,
+    Ardei,
+    Ceapa,
+    Masline,
+    Sunca,
+    Chorizzo,
+    Ananas
+
+}
+
+public class Pizza
+{
+    public readonly ReadOnlyCollection<string> Ingredients;
+
+    public Pizza(Stack<string> ingredients)
+    {
+        Ingredients = ingredients.ToList().AsReadOnly();
+    }
+
+    public void Print()
+    {
+        var table = new Table();
+        table.Border(TableBorder.Ascii);
+
+        table.AddColumn("Pizza the side view:");
+        foreach (var ingredient in Ingredients)
+        {
+            table.AddRow(ingredient);
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
