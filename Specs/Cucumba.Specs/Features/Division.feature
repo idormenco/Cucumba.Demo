5faacf135402fc961adb8245ad4fe4262fc7d873
Ion Dormenco <idormenco@gmail.com>
2023-11-09 10:33:57 +0200

    division

---
+++ Specs/Cucumba.Specs/Features/Division.feature
@@ -0,0 +1,13 @@
+﻿Feature: Calculator divide
+
+Scenario Outline: Division
+    Given the first number is <First>
+    And the second number is <Second>
+    When the two numbers are divided
+    Then the result should be <Result>
+Examples:
+    | First | Second | Result |
+    |  6    |  3     |  2     | 
+    |  8    |  4     |  2     | 
+    |  1    |  1     |  1     | # element neutru
+    | 15    | -3     | -5     |
---
+++ Specs/Cucumba.Specs/StepDefinitions/DivisionStepDefinition.cs
@@ -0,0 +1,35 @@
+﻿using Cucumba.Demo.Calculator;
+
+namespace Cucumba.Specs.StepDefinitions;
+
+[Binding]
+[Scope(Feature = "Calculator divide")]
+public class DivisionStepDefinition
+{
+    private readonly IAmCalculator _calculator = Dear.Calculator;
+    private double _result;
+
+    [Given(@"the first number is (.*)")]
+    public void GivenTheFirstNumberIs(int firstNumber)
+    {
+        _calculator.Add(firstNumber);
+    }
+
+    [Given(@"the second number is (.*)")]
+    public void GivenTheSecondNumberIs(int secondNumber)
+    {
+        _calculator.DivideBy(secondNumber);
+    }
+
+    [When(@"the two numbers are divided")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        _result = _calculator.Please();
+    }
+
+    [Then(@"the result should be (.*)")]
+    public void ThenTheResultShouldBe(int expectedResult)
+    {
+        _result.Should().Be(expectedResult);
+    }
+}
